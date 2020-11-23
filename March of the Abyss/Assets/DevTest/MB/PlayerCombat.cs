d993ee27ec6a5da337a71d18029be021d5cc0c0e
Manuelbonnet90 <58072925+Manuelbonnet90@users.noreply.github.com>
2020-11-23 10:54:12 +0000

    added asest

---
+++ March of the Abyss/Assets/DevTest/MB/PlayerCombat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+    Animator animator;
+
+    public Collider[] attackHitboxes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            LaunchMeeleAttack(attackHitboxes[0]);
+            animator.SetTrigger("Attack1");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            LaunchMeeleAttack(attackHitboxes[1]);
+            animator.SetTrigger("Attack2");
+        }
+    }
+
+    void LaunchMeeleAttack(Collider col)
+    {
+        Collider[] cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, col.transform.rotation, LayerMask.GetMask("Hitbox"));
+        foreach (Collider c in cols)
+            Debug.Log(c.name);
+    }
+}
