dcbde9e505a497e314a2ff51b6d24d6660577604
Olafs <58072455+UEL-TeamPlaceholder@users.noreply.github.com>
2021-02-17 16:52:31 +0000

    shop ui

---
+++ March of the Abyss/Assets/DevTest/OM/Scripts/LightControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour
+{
+    
+
+        public float cycleloop = 5f;
+        public float cyclespeed;
+
+        void Start()
+        {
+            cyclespeed = 0.1f / cycleloop * -1f;
+        }
+
+        void Update()
+        {
+            transform.Rotate(0, 0, cyclespeed, Space.World);
+        }
+    
+}
